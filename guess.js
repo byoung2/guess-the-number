cb70fe716b2b464cddec91a8ec2a477ad0ddc306
Bryan Young <byoung2@gmail.com>
2018-10-30 09:50:10 -0700

    Initial commit: greate basic guess number game

---
+++ guess.js
@@ -0,0 +1,76 @@
+const _ = require('lodash');
+const input = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+/**
+ * Class GuessNumberGame
+ *
+ * Constructor takes a max number (e.g. 1-100 or 1-1000, etc) so game difficuty
+ * can be adjusted. Also allows picking of number of tries
+ *
+ */
+class GuessNumberGame {
+  /**
+   * @param int max maximum range for guessed number
+   * @param int numTries the number of tries before the game ends
+   *
+   */
+  constructor(max, numTries) {
+    this.target = _.random(1, max);
+    this.numTries = numTries;
+    this.history = [];
+    console.log(`Try to guess the number (1 to ${ max }). You have ${ numTries } guesses.`);
+  }
+
+  /**
+   * @param int num the player's guess.
+   *
+   */
+  guess(num) {
+    // Do not penalize the player for incorrect input
+    if(!num.match(/^[0-9]+$/gi)) {
+      console.log(`${ num } is not a valid number. Please try again. You still have ${ this.numTries } guesses.`);
+      return false;
+    }
+
+    num = parseInt(num);
+    // Do not penalize the player for duplicate guesses
+    if(_.find(this.history, item => {
+      return item === num;
+    })) {
+      console.log(`You already guessed ${ num }. Please try again. You still have ${ this.numTries } guesses.`);
+      return false;
+    }
+
+    // Store a history of guesses
+    this.history.push(num);
+    if(num === this.target) {
+      console.log(`${ num } was the correct answer! Congratulations!`);
+      process.exit(0);
+    }
+
+    this.numTries--;
+    if(this.numTries === 0) {
+      console.log(`Sorry, you have no more tries remaining.`);
+      process.exit(0);
+    }
+    let hint;
+    if(num > this.target) {
+      hint = 'too high';
+    } else {
+      hint = 'too low';
+    }
+    console.log(`${ num } was not the correct number (your guess was ${ hint }). ${ this.numTries } guesses remaining.`);
+    console.log('Previous guesses:', this.history);
+  }
+}
+
+let game = new GuessNumberGame(100, 10);
+
+input.on('line', function(line) {
+  game.guess(line);
+
+});
